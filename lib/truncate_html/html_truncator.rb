7017e610e8f4a2e8859e6c46c33f0da50e92ef3b
Harold Giménez <harold.gimenez@gmail.com>
2009-08-02 14:25:53 -0400

    Moved logic to its own class. Improved spec coverage of more edge cases and malformed HTML. Added ability to handle unpaired HTML tags (br, hr and img).

---
+++ lib/truncate_html/html_truncator.rb
@@ -0,0 +1,95 @@
+module TruncateHtml
+  class HtmlTruncator
+
+    UNPAIRED_TAGS = %w(br hr img)
+
+    def initialize(original_html)
+      @original_html = original_html
+    end
+
+    def truncate(options = {})
+      options[:length] ||= 100
+      options[:omission] ||= '...'
+      @chars_remaining = options[:length]
+      @open_tags, result = [], []
+
+      html_tokens.each do |str|
+        if @chars_remaining > 0
+          if html_tag?(str)
+            if open_tag?(str)
+              @open_tags << str 
+            else
+              open_tags = remove_latest_open_tag(str)
+            end
+          else
+            @chars_remaining -= (str.length + 1)
+          end
+          result << str
+        else
+          result[-1] += options[:omission] unless options[:omission].nil?
+          @open_tags.reverse.each do |open_tag|
+            result << matching_close_tag(open_tag)
+          end
+          break
+        end
+      end
+      construct_html_from_truncated_tokens(result)
+    end
+
+
+    private #############################
+
+    def html_tokens
+      @original_html.scan(/<\/?[^>]+>|[\w\|`~!@#\$%^&*\(\)\-_\+=\[\]{}:;'",\.\/?]+/).map do
+        |t| t.gsub(
+          #remove newline characters
+            /\n/,''
+        ).gsub(
+          #clean out extra consecutive whitespace
+            /\s+/, ' '
+        )
+      end 
+    end
+
+    def html_tag?(string)
+      if string =~ /<\/?[^>]+>/ then true else false end
+    end
+
+    def open_tag?(html_tag)
+      if html_tag =~ /<(?!(?:#{UNPAIRED_TAGS.join('|')}|\/))[^\/>]+>/i then true else false end
+    end
+
+    def remove_latest_open_tag(close_tag)
+      (0...@open_tags.length).to_a.reverse.each do |i|
+        if matching_close_tag(@open_tags[i]) == close_tag
+          @open_tags.delete_at(i)
+          break
+        end
+      end
+    end
+
+    def matching_close_tag(open_tag)
+      open_tag.gsub(/<(\w+)\s?.*>/, '</\1>').strip
+    end
+
+    def construct_html_from_truncated_tokens(tokens)
+      tokens.join(' ').gsub(
+        #remove whitespace between word and closing html tag
+        /([\w`~!@#\$%^&*\(\)\-_\+=\[\]{}:;'",\.\/?]+?)\s+(<\/[^>]*>)/, '\1\2'
+      ).gsub(
+        #remove whitespace between closing tag and punctuation
+        /(<\/[^>]*>)\s+([`~!@#\$%^&*\(\)\-_\+=\[\]{}:;'",\.\/?]+?)/, '\1\2'
+      ).gsub(
+        #remove whitespace between open tag and word chars.
+        /(<[^>]*>)\s+([`~!@#\$%^&*\(\)\-_\+=\[\]{}:;'",\.\/?]+?)/, '\1\2'
+      ).gsub(
+        #remove whitespace between tags
+        /(>)\s+(<)/, '\1\2'
+      ).gsub(
+        #remove whitespace between tag and word
+        /(>)\s+(\w+)/, '\1\2'
+      )
+    end
+
+  end
+end
--- spec/helpers/truncate_html_helper_spec.rb
+++ spec/helpers/truncate_html_helper_spec.rb
@@ -4,91 +4,74 @@ include TruncateHtmlHelper
 
 describe TruncateHtmlHelper do
 
+  it 'should be included in ActionView::Base' do
+    ActionView::Base.included_modules.should include(TruncateHtmlHelper)
+  end
+
   describe '#truncate_html' do
 
-    before(:each) do
-      @html = '<ul><li><a href="foo">I\'m a link</a><li></ul>'
+    context 'truncating in the middle of a link' do
+      before(:each) do
+        @html = '<div><ul><li>Look at <a href="foo">this</a> link <li></ul></div>'
+      end
+
+      it 'should truncate, and close the <a>, and close any remaining open tags' do
+        truncate_html(@html, :length => 10).should == '<div><ul><li>Look at <a href="foo">this...</a></li></ul></div>'
+      end
     end
 
-    it 'should truncate, and close the <a>, <li> and <ul> tags' do
-      truncate_html(@html, :length => 5).should == '<ul> <li> <a href="foo"> I\'m a...</a> </li> </ul>'
-    end
-  end
-
-  describe '#html_tokens' do
-    before(:each) do
-      @html = <<-END_HTML
-        <h1>Hi there</h1>
-        <p>This is sweet!</p>
-      END_HTML
+    %w(! @ # $ % ^ & * \( \) - _ + = [ ] { } \ | , . / ?).each do |char|
+      context "when the html has a #{char} character after a closing tag" do
+        it 'should place the punctuation after the tag without any whitespace' do
+          @html = "<p>Look at <strong>this</strong>#{char} More words here</p>"
+          truncate_html(@html, :length => 16).should == "<p>Look at <strong>this</strong>#{char} More...</p>"
+        end
+      end
     end
 
-    it 'should return each token in the string as an array element' do
-      html_tokens(@html).should == ['<h1>', 'Hi', 'there','</h1>','<p>','This','is','sweet!','</p>']
-    end
-  end
+    #unusual, but just covering my ass
+    context 'when the HTML tags are multiline' do 
+      before(:each) do
+        @html = <<-END_HTML
+          <div id="foo"
+                class="bar">
+          This is ugly html.
+          </div>
+        END_HTML
+      end
 
-  describe '#html_tag?' do
-    it 'should be false when the string parameter is not an html tag' do
-      html_tag?('no tags').should be_false
-    end
-
-    it 'should be true when the string parameter is an html tag' do
-      html_tag?('<img src="foo">').should be_true
-    end
-  end
-
-  describe '#open_tag?' do
-    it 'should be true if the tag is an open tag' do
-      open_tag?('<a>').should be_true
+      it 'should recognize the multiline html properly' do
+        truncate_html(@html, :length=> 6).should == '<div id="foo" class="bar">This is...</div>'
+      end
     end
 
-    it 'should be true if the tag is an open tag, and has whitespace and html properties with either single or double quotes' do
-      open_tag?(' <a href="whatever">').should be_true
-      open_tag?(" <a href='whatever' >").should be_true
-    end
+    %w(br hr img).each do |unpaired_tag|
+      context "when the html contains a #{unpaired_tag} tag" do
 
-    it 'should be false if the tag is a close tag' do
-      open_tag?('</a>').should be_false
-    end
+        context "and the #{unpaired_tag} does not have the closing slash" do
+          before(:each) do
+            @html = "<div>Some before. <#{unpaired_tag}>and some after</div>"
+            @html_caps = "<div>Some before. <#{unpaired_tag.capitalize}>and some after</div>"
+          end
+          it "should not close the #{unpaired_tag} tag" do
+            truncate_html(@html, :length => 15).should == "<div>Some before. <#{unpaired_tag}>and...</div>"
+            truncate_html(@html_caps, :length => 15).should == "<div>Some before. <#{unpaired_tag.capitalize}>and...</div>"
+          end
+        end
 
-    it 'should be false if the string isnot an html tag' do
-      open_tag?('random stuff').should be_false
-    end
-  end
+        context "and the #{unpaired_tag} does not have the closing slash" do
+          before(:each) do
+            @html = "<div>Some before. <#{unpaired_tag} />and some after</div>"
+            @html_caps = "<div>Some before. <#{unpaired_tag.capitalize} />and some after</div>"
+          end
+          it "should not close the #{unpaired_tag} tag" do
+            truncate_html(@html, :length => 15).should == "<div>Some before. <#{unpaired_tag} />and...</div>"
+            truncate_html(@html_caps, :length => 15).should == "<div>Some before. <#{unpaired_tag.capitalize} />and...</div>"
+          end
+        end
 
-  describe '#escape_special_chars' do
-    ['^', '$', '.', '|', '?', '*', '+', '-'].each do |char|
-      it "should backslash a #{char}" do
-        escape_special_chars("some_text_with_a_#{char}_on_it").should == "some_text_with_a_\\" + char + "_on_it"
       end
     end
   end
-
-  describe '#matching_close_tag' do
-    it 'should close a tag given an open tag' do
-      matching_close_tag('<a>').should == '</a>'
-      matching_close_tag(' <div>').should == '</div>'
-      matching_close_tag('<h1>').should == '</h1>'
-      matching_close_tag('<a href="foo">').should == '</a>'
-    end
-  end
-
-  describe '#remove_open_tag' do
-    before(:each) do
-      @open_tags = ['<a>', '<p>', '<div>', '<h1>', '<div>']
-    end
-
-    it 'should remove the latest matching tag' do
-      remove_latest_open_tag(@open_tags, '</h1>').should == ['<a>', '<p>', '<div>', '<div>']
-      remove_latest_open_tag(@open_tags, '</div>').should == ['<a>', '<p>', '<div>', '<h1>']
-    end
-
-    it 'should return the open_tags array if the close tag didn not match' do
-      remove_latest_open_tag(@open_tags, '<strong>').should == @open_tags
-    end
-  end
-
-  
 
 end
