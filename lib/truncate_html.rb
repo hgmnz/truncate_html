7bfbb2b91024bd09fe41dbf731c01f29d8f11e95
Harold Giménez <harold.gimenez@gmail.com>
2010-01-17 13:08:46 -0500

    Cosmetic - whitespace

--- lib/truncate_html.rb
+++ lib/truncate_html.rb
@@ -1,7 +1,7 @@
-path =  File.join(File.dirname(__FILE__), 'app', 'helpers')  
-$LOAD_PATH << path 
-ActiveSupport::Dependencies.load_paths << path 
-ActiveSupport::Dependencies.load_once_paths.delete(path) 
+path =  File.join(File.dirname(__FILE__), 'app', 'helpers')
+$LOAD_PATH << path
+ActiveSupport::Dependencies.load_paths << path
+ActiveSupport::Dependencies.load_once_paths.delete(path)
 
 require File.join(File.dirname(__FILE__), 'truncate_html', 'html_truncator')
 
