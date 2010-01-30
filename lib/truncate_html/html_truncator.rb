9396b63dbf39a3dc56b64ad9a474292d35cea0f4
Harold Giménez <harold.gimenez@gmail.com>
2010-01-30 13:32:19 -0500

    Refactoring parsing methods.
    Created the HtmlParser module which now is included in HtmlTruncator.

--- lib/truncate_html/html_truncator.rb
+++ lib/truncate_html/html_truncator.rb
@@ -1,10 +1,10 @@
 module TruncateHtml
   class HtmlTruncator
+    include HtmlParser
 
-    UNPAIRED_TAGS = %w(br hr img)
 
     def initialize(original_html)
-      @original_html = original_html
+      @original_html  = original_html
     end
 
     def truncate(options = {})
@@ -14,13 +14,13 @@ module TruncateHtml
       @chars_remaining = options[:length] - options[:omission].length
       @open_tags, result = [], ['']
 
-      html_tokens.each do |str|
+      html_tokens(@original_html).each do |str|
         if @chars_remaining > 0
           if html_tag?(str)
             if open_tag?(str)
               @open_tags << str
             else
-              open_tags = remove_latest_open_tag(str)
+              remove_latest_open_tag(str)
             end
           else
             @chars_remaining -= str.length
@@ -36,39 +36,5 @@ module TruncateHtml
       end
       result.join('')
     end
-
-    def html_tokens
-      @original_html.scan(/(?:<script.*>.*<\/script>)+|<\/?[^>]+>|[\w\|`~!@#\$%^&*\(\)\-_\+=\[\]{}:;'",\.\/?]+|\s+/).map do
-        |t| t.gsub(
-          #remove newline characters
-            /\n/,''
-        ).gsub(
-          #clean out extra consecutive whitespace
-            /\s+/, ' '
-        )
-      end
-    end
-
-    def html_tag?(string)
-      string =~ /<\/?[^>]+>/ ? true : false
-    end
-
-    def open_tag?(html_tag)
-      html_tag =~ /<(?!(?:#{UNPAIRED_TAGS.join('|')}|script|\/))[^>]+>/i ? true : false
-    end
-
-    def remove_latest_open_tag(close_tag)
-      (0...@open_tags.length).to_a.reverse.each do |i|
-        if matching_close_tag(@open_tags[i]) == close_tag
-          @open_tags.delete_at(i)
-          break
-        end
-      end
-    end
-
-    def matching_close_tag(open_tag)
-      open_tag.gsub(/<(\w+)\s?.*>/, '</\1>').strip
-    end
-
   end
 end
