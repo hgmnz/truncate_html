ce044d811d5dbb02731cc71eb6bce45744b346b1
Kevin Ball <kball@causes.com>
2010-03-22 15:45:19 -0700

    Work around a failure to autoload classes in frozen gems in Rails 2.1 (and make load more robust)
    Signed-off-by: Harold Giménez <harold.gimenez@gmail.com>

--- lib/truncate_html.rb
+++ lib/truncate_html.rb
@@ -1,4 +1,5 @@
 require File.join(File.dirname(__FILE__), 'truncate_html', 'html_truncator')
+require File.join(File.dirname(__FILE__), 'truncate_html', 'html_string')
 require File.join(File.dirname(__FILE__), 'truncate_html', 'configuration')
 require File.join(File.dirname(__FILE__), 'app', 'helpers', 'truncate_html_helper')
 
