6efe261b891cc2dceb77177857f63e8ab4e5d958
Harold Giménez <harold.gimenez@gmail.com>
2012-04-24 09:29:46 -0700

    Fix indentation

--- lib/truncate_html/html_truncator.rb
+++ lib/truncate_html/html_truncator.rb
@@ -14,7 +14,6 @@ module TruncateHtml
 
       return @omission if @chars_remaining < 0
       @original_html.html_tokens.each do |token|
-        #if truncate_more?(token)
         if @chars_remaining > 0
           process_token(token)
         else
@@ -27,46 +26,46 @@ module TruncateHtml
 
     private
 
-      def process_token(token)
-        append_to_result(token)
-        if token.html_tag?
-          if token.open_tag?
-            @open_tags << token
-          else
-            remove_latest_open_tag(token)
-          end
+    def process_token(token)
+      append_to_result(token)
+      if token.html_tag?
+        if token.open_tag?
+          @open_tags << token
         else
-          @chars_remaining -= (@word_boundary ? token.length : token[0, @chars_remaining].length)
-          if @chars_remaining <= 0
-            @truncated_html[-1] = @truncated_html[-1].rstrip + @omission
-          end
+          remove_latest_open_tag(token)
+        end
+      else
+        @chars_remaining -= (@word_boundary ? token.length : token[0, @chars_remaining].length)
+        if @chars_remaining <= 0
+          @truncated_html[-1] = @truncated_html[-1].rstrip + @omission
         end
       end
+    end
 
-      def append_to_result(token)
-        if token.html_tag?
-          @truncated_html << token
-        elsif @word_boundary
-          @truncated_html << token if (@chars_remaining - token.length) >= 0
-        else
-          @truncated_html << token[0, @chars_remaining]
-        end
+    def append_to_result(token)
+      if token.html_tag?
+        @truncated_html << token
+      elsif @word_boundary
+        @truncated_html << token if (@chars_remaining - token.length) >= 0
+      else
+        @truncated_html << token[0, @chars_remaining]
       end
+    end
 
-      def close_open_tags
-        @open_tags.reverse_each do |open_tag|
-          @truncated_html << open_tag.matching_close_tag
-        end
+    def close_open_tags
+      @open_tags.reverse_each do |open_tag|
+        @truncated_html << open_tag.matching_close_tag
       end
+    end
 
-      def remove_latest_open_tag(close_tag)
-        (0...@open_tags.length).to_a.reverse.each do |index|
-          if @open_tags[index].matching_close_tag == close_tag
-            @open_tags.delete_at(index)
-            break
-          end
+    def remove_latest_open_tag(close_tag)
+      (0...@open_tags.length).to_a.reverse.each do |index|
+        if @open_tags[index].matching_close_tag == close_tag
+          @open_tags.delete_at(index)
+          break
         end
       end
+    end
 
   end
 end
