18d6a0eb1fc50383d8b184649df60dc3e3200a46
Harold Giménez <harold.gimenez@gmail.com>
2013-01-04 21:40:59 -0800

    Refactor to make truncation options part of the constructor params

--- lib/app/helpers/truncate_html_helper.rb
+++ lib/app/helpers/truncate_html_helper.rb
@@ -3,7 +3,7 @@ module TruncateHtmlHelper
   def truncate_html(html, options={})
     return '' if html.nil?
     html_string = TruncateHtml::HtmlString.new(html)
-    TruncateHtml::HtmlTruncator.new(html_string).truncate(options).html_safe
+    TruncateHtml::HtmlTruncator.new(html_string, options).truncate.html_safe
   end
 
 end
