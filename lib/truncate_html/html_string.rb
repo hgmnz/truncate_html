c950cf764cb3628a0692ad0c746e205dfa560de2
Nisanth Chunduru <nisanth074@gmail.com>
2014-12-27 00:22:14 +0530

    Add comments explaining the token regex

--- lib/truncate_html/html_string.rb
+++ lib/truncate_html/html_string.rb
@@ -10,6 +10,15 @@ module TruncateHtml
       |
       \s+ # Match consecutive spaces. They are later truncated to a single space.
       |
+      # [[:alpha]] - Match unicode alphabetical characters
+      # \p{Sc} - Match unicode currency characters
+      # \p{So} - Match unicode other symbols
+      # [\p{Sm}&&[^<]] - Match unicode math characters except ASCII <. < opens html tags.
+      # [\p{Zs}&&[^\s]] - Match unicode space characters except \s+. We truncate consecutive normal spaces.
+      # [0-9]\|`~!@#\$%^&*\(\)\-_\+=\[\]{}:;'²³§",\.\/? - Match digits, few more characters
+      # [[:punct]] - Don't gobble up chinese punctuation characters
+      #
+      # Refer to ruby's regex docs (http://www.ruby-doc.org/core-1.9.3/Regexp.html) for more info
       [[[:alpha:]]\p{Sc}\p{So}[\p{Sm}&&[^<]][\p{Zs}&&[^\s]][0-9]\|`~!@#\$%^&*\(\)\-_\+=\[\]{}:;'²³§",\.\/?[[:punct:]]]+ # Match tag body
     }x.freeze
 
