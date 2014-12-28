9000c427985d570ed2168419380203403e7c3ccd
Nisanth Chunduru <nisanth074@gmail.com>
2014-12-28 15:41:50 +0530

    Extract valid tag body characters into a separate variable

--- lib/truncate_html/html_string.rb
+++ lib/truncate_html/html_string.rb
@@ -3,6 +3,15 @@ module TruncateHtml
   class HtmlString < String
 
     UNPAIRED_TAGS = %w(br hr img).freeze
+    TAG_BODY_CHARACTERS =
+      '[[:alpha:]]' + # Match unicode alphabetical characters
+      '\p{Sc}' + # Match unicode currency characters
+      '\p{So}' + # Match unicode other symbols
+      '[\p{Sm}&&[^<]]' + # Match unicode math symbols except ascii <. < opens html tags.
+      '[\p{Zs}&&[^\s]]' + # Match unicode space characters except \s+
+      '[0-9]' + # Match digits
+      %q(\|`~!@#\$%^&*\(\)\-_\+=\[\]{}:;'²³§",\.\/?) + # Match some special characters
+      '[[:punct:]]' # Don't gobble up chinese punctuation characters
     REGEX = %r{
       (?:<script.*>.*<\/script>)+ # Match script tags. They aren't counted in length.
       |
@@ -10,16 +19,7 @@ module TruncateHtml
       |
       \s+ # Match consecutive spaces. They are later truncated to a single space.
       |
-      # [[:alpha]] - Match unicode alphabetical characters
-      # \p{Sc} - Match unicode currency characters
-      # \p{So} - Match unicode other symbols
-      # [\p{Sm}&&[^<]] - Match unicode math characters except ASCII <. < opens html tags.
-      # [\p{Zs}&&[^\s]] - Match unicode space characters except \s+. We truncate consecutive normal spaces.
-      # [0-9]\|`~!@#\$%^&*\(\)\-_\+=\[\]{}:;'²³§",\.\/? - Match digits, few more characters
-      # [[:punct]] - Don't gobble up chinese punctuation characters
-      #
-      # Refer to ruby's regex docs (http://www.ruby-doc.org/core-1.9.3/Regexp.html) for more info
-      [[[:alpha:]]\p{Sc}\p{So}[\p{Sm}&&[^<]][\p{Zs}&&[^\s]][0-9]\|`~!@#\$%^&*\(\)\-_\+=\[\]{}:;'²³§",\.\/?[[:punct:]]]+ # Match tag body
+      [#{TAG_BODY_CHARACTERS}]+ # Match tag body
     }x.freeze
 
     def initialize(original_html)
